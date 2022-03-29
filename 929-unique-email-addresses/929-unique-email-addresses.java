15c50fcd24123d137c53cecf3caff1c7840f1c41
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-03-30 03:23:52 +0530

    Update 929-unique-email-addresses.java

--- 929-unique-email-addresses/929-unique-email-addresses.java
+++ 929-unique-email-addresses/929-unique-email-addresses.java
@@ -1,11 +1,18 @@
 class Solution {
     public int numUniqueEmails(String[] emails) {
+        //set for storing valid emails
         Set<String> set= new HashSet<>();
         for(String email:emails){
+            //split in 0 and 1 from @ 
+            //0 = local part
+            //1 = domain part;
             String[] parts= email.split("@");
+            //0= part before +, rest is ignored
             String[] local= parts[0].split("\\+");
+            //removing dot and adding @ and domain
             set.add(local[0].replace(".","")+"@"+parts[1]);
         }
+        //returning how many are present in hashset
         return set.size();
         
     }
