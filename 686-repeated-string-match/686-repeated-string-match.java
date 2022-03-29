41e4a612543810e4369b64e2d71cc7f87dc6c27a
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-03-30 02:22:44 +0530

    Time: 375 ms (30.04%), Space: 113.7 MB (18.11%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.java
+++ 686-repeated-string-match/686-repeated-string-match.java
@@ -1,23 +1,13 @@
 class Solution {
 
     public int repeatedStringMatch(String a, String b) {
-        //creating new string 'sa' to add in 'a' later on
-        String sa = a;
-        //dividing 'b' by 'a', which gives the number of times 'a' would be present in 'b'
-        //repeat= number of repeatitions of a in b
+        StringBuilder sb = new StringBuilder();
         int repeat = b.length() / a.length();
-        int count = 1;
-        // repeat+2 because 2 more substri is possible at the start and at the end
-        for (int i = 0; i < repeat + 2; i++) {
-            if (a.contains(b)) {
-                return count;
-            } else {
-                //adding a in a and again
-                a += sa;
-                count++;
+        for (int i = 1; i <= repeat+2; i++) {
+            if (sb.append(a).toString().contains(b)) {
+                return i;
             }
         }
-        //if no substri is there
         return -1;
     }
 }
