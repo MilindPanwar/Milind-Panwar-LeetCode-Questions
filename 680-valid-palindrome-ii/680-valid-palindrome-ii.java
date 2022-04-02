5009d28305e31510a8b9496a51a0c15211dd1050
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-04-02 16:44:08 +0530

    Time: 11 ms (45.81%), Space: 54.1 MB (74.17%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -1,11 +1,13 @@
 class Solution {
 
     public static boolean pal(String s, int start, int end) {
+        //recursion
         while (start < end) {
             if (s.charAt(start) == s.charAt(end)) {
                 start++;
                 end--;
             } else {
+                //if it is still not equal return false
                 return false;
             }
         }
@@ -23,6 +25,7 @@ class Solution {
                 //if case 1 is false another, case 2 is checked
                 // if both are false , false is returned
                 return pal(s, start + 1, end) || pal(s, start, end - 1);
+                //above pal 1st one is true then it is chosen otherwise 2nd one is chosen
             }
         }
         return true;
