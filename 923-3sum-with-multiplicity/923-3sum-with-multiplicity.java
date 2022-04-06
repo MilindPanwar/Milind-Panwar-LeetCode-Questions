5ce774daf56cfa9c951f7a08e1ed06b98997de85
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-04-07 00:24:41 +0530

    Time: 1 ms (100.00%), Space: 41.2 MB (100.00%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -1,25 +1,26 @@
 class Solution {
 
     public int threeSumMulti(int[] arr, int target) {
+        int mod = 1_000_000_007;
         long result = 0;
-        long count[] = new long[101];
-        long mod = 1000000007;
-
-        for (int i : arr) count[i]++;
-        for (int i = 0; i < 101; i++) {
-            for (int j = i; j < 101; j++) {
+        long[] c = new long[101];
+        for (int i : arr) {
+            c[i]++;
+        }
+        for (int i = 0; i <= 100; i++) {
+            for (int j = i; j <=100; j++) {
                 int k = target - i - j;
-                if (k < 0 || k > 100) continue;
-                if (i == j && j == k) {
-                    result += (count[i] * (count[i] - 1) * (count[i] - 2)) / 6;
+                if (k < 0 || k > 100) {
+                    continue;
+                } else if (i == j && j == k) {
+                    result += (c[i] * (c[i] - 1) * (c[i] - 2)/6);
                 } else if (i == j && j != k) {
-                    result += ((count[i] * (count[i] - 1)) / 2) * count[k];
+                    result += (c[i] * (c[i] - 1) / 2) * c[k];
                 } else if (i < j && j < k) {
-                    result += (count[i] * count[j] * count[k]);
+                    result += c[i] * c[j] * c[k];
                 }
-                result %= mod;
             }
         }
-        return (int) result;
+        return (int) (result % mod);
     }
 }
