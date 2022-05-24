86e6c15dd7159764fd0bccb77988f2708a07e3e2
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-05-24 14:33:56 +0530

    Time: 1 ms (91.65%), Space: 54.4 MB (20.10%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,37 +1,24 @@
 class Solution {
 
     public void setZeroes(int[][] matrix) {
-        int col0 = 1, row = matrix.length, col = matrix[0].length;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[][] up = new int[1][col];
+        int[][] left = new int[row][1];
 
         for (int i = 0; i < row; i++) {
-            //if matrix[0][0] is 0 then put col0 as false
-            if (matrix[i][0] == 0) {
-                col0 = 0;
-            }
-            for (int j = 1; j < col; j++) {
-                //if element at that i j index is 0
+            for (int j = 0; j < col; j++) {
                 if (matrix[i][j] == 0) {
-                    //assign leftmost as 0
-                    matrix[i][0] = 0;
-                    //assign topmost as 0
-                    matrix[0][j] = 0;
+                    up[0][j] = -1;
+                    left[i][0] = -1;
                 }
             }
         }
-        //traversing from back
-        for (int i = row - 1; i >= 0; i--) {
-            //traversing till j>=1
-            for (int j = col - 1; j >= 1; j--) {
-                //if leftmost and topmost is 0
-                if (matrix[i][0] == 0 || matrix[0][j]==0) {
-                    //assign i j element as 0
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (up[0][j]==-1 || left[i][0]==-1) {
                     matrix[i][j] = 0;
                 }
-            }
-            //if col0 is false
-            if (col0 == 0) {
-                //assign zero for all columns of i row
-                matrix[i][0] = 0;
             }
         }
     }
