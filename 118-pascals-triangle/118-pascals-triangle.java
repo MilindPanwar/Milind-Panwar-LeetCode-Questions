3af5dc7754c1061377a14e22d5aea5484a120bf9
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-06-06 19:54:24 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (81.42%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,31 +1,38 @@
 class Solution {
+
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>>result = new ArrayList<>();
+        //list of list of integers
+        List<List<Integer>> l = new ArrayList<>();
         
-        List<Integer> l1=new ArrayList<>();
+        //l1 for first row consisting on only 1
+        List<Integer> l1 = new ArrayList<>();
         l1.add(1);
-        result.add(l1);
-        if(numRows==1){
-            return result;
+        l.add(l1);
+        if (numRows == 1) {
+            return l;
         }
-        
-        List<Integer> l2= new ArrayList<>();
+        //l2 for second row consisting only 2
+        List<Integer> l2 = new ArrayList<>();
         l2.add(1);
         l2.add(1);
-        result.add(l2);
-        if(numRows==2){
-            return result;
+        l.add(l2);
+        if (numRows == 2) {
+            return l;
         }
         
-        for(int i=2;i<numRows;i++){
-            List<Integer> li=new ArrayList<>();
+        //starting i from 2, as 1 and 2 row are done
+        for (int i = 2; i < numRows; i++) {
+            
+            List<Integer> li = new ArrayList<>();
             li.add(1);
-            for(int j=1;j<i;j++){
-                li.add(j,result.get(i-1).get(j-1)+result.get(i-1).get(j));
+            for (int j = 1; j < i; j++) {
+                //get i-1 . get j-1 means, get previous row previous col element
+                //get i-1 .get j means, get previous row current col element
+                li.add(j, l.get(i - 1).get(j - 1) + l.get(i - 1).get(j));
             }
             li.add(1);
-            result.add(li);
+            l.add(li);
         }
-        return result;
+        return l;
     }
 }
