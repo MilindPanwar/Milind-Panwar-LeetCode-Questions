e40ac2bee09df68a4c784075f737b83abedc5722
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-06-07 20:08:29 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (5.79%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -4,20 +4,15 @@ class Solution {
         int p1 = m - 1;
         int p2 = n - 1;
         int i = m + n - 1;
-//while terminates when p2 is lesser than zero
         while (p2 >= 0) {
-            
-//if executes till p1 is greater or equal to zero
             if (p1 >= 0 && nums1[p1] > nums2[p2]) {
-//if p1 is greater than p2
                 nums1[i] = nums1[p1];
+                i--;
                 p1--;
-                i--;
             } else {
-//if p2 is greater than p1
                 nums1[i] = nums2[p2];
-                p2--;
                 i--;
+                p2--;
             }
         }
     }
