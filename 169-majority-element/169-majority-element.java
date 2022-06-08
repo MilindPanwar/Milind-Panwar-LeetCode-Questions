39cac55092e210a73bd157d3b149ec513396a204
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-06-08 19:41:49 +0530

    Time: 14 ms (25.87%), Space: 47.6 MB (76.16%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,6 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        Map<Integer,Integer> hm= new HashMap<Integer, Integer>();
+        int res=0;        
+        for(int i : nums)
+        {
+            if(!hm.containsKey(i)){
+                //adding number if not present
+                hm.put(i,1);
+            }else{ 
+                //incrementing key value if number is present
+                hm.put(i,hm.get(i)+1);
+            }
+            if(hm.get(i)>nums.length/2){
+                //i means the number so storing the number in result
+                res=i;
+                break;
+            }
+        }
+        return res;
     }
 }
