90d873a4e0015772008c6dd2adce57ae5c82859c
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-06-09 15:19:46 +0530

    Time: 4 ms (69.55%), Space: 44.8 MB (62.97%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,12 +1,48 @@
 class Solution {
 
     public int[] twoSum(int[] nums, int target) {
+        if(nums==null){
+            return null;
+        }
+        int[] nid = new int[nums.length];
+        for (int j = 0; j < nums.length; j++) {
+            nid[j] = nums[j];
+        }
+        int st = 0;
+        Arrays.sort(nums);
+        int en = nums.length - 1;
         int[] arr = new int[2];
+        int num1 = 0;
+        int num2 = 0;
+        while (st < en) {
+            if (nums[st] + nums[en] == target) {
+                num1 = nums[st];
+                num2 = nums[en];
+                break;
+            } else if (nums[st] + nums[en] > target) {
+                en--;
+            } else {
+                st++;
+            }
+        }
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if ((nums[i] + nums[j]) == target) {
-                    arr[0] = i;
-                    arr[1] = j;
+            if (num1 == nid[i]) {
+                arr[0] = i;
+                break;
+            }
+        }
+        if (num1 !=num2) {
+            for (int i = 0; i < nums.length; i++) {
+                if (num2 == nid[i]) {
+                    arr[1] = i;
+                    break;
+                }
+            }
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (num2 == nid[i] && i != arr[0]) {
+                    arr[1] = i;
+                    break;
                 }
             }
         }
