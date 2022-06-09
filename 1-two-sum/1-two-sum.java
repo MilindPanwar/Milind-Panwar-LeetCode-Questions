27efb2fa7d4e5c07c77bfc533b7c1006d138a854
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-06-09 15:50:58 +0530

    Time: 1 ms (99.65%), Space: 42.4 MB (89.92%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,49 +1,15 @@
 class Solution {
 
     public int[] twoSum(int[] nums, int target) {
-        if(nums==null){
-            return null;
-        }
-        int[] nid = new int[nums.length];
-        for (int j = 0; j < nums.length; j++) {
-            nid[j] = nums[j];
-        }
-        int st = 0;
-        Arrays.sort(nums);
-        int en = nums.length - 1;
+        Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
         int[] arr = new int[2];
-        int num1 = 0;
-        int num2 = 0;
-        while (st < en) {
-            if (nums[st] + nums[en] == target) {
-                num1 = nums[st];
-                num2 = nums[en];
-                break;
-            } else if (nums[st] + nums[en] > target) {
-                en--;
-            } else {
-                st++;
-            }
-        }
         for (int i = 0; i < nums.length; i++) {
-            if (num1 == nid[i]) {
+            if (mp.containsKey(target - nums[i])) {
                 arr[0] = i;
-                break;
-            }
-        }
-        if (num1 !=num2) {
-            for (int i = 0; i < nums.length; i++) {
-                if (num2 == nid[i]) {
-                    arr[1] = i;
-                    break;
-                }
-            }
-        } else {
-            for (int i = 0; i < nums.length; i++) {
-                if (num2 == nid[i] && i != arr[0]) {
-                    arr[1] = i;
-                    break;
-                }
+                arr[1] = mp.get(target - nums[i]);
+                return arr;
+            } else {
+                mp.put(nums[i], i);
             }
         }
         return arr;
