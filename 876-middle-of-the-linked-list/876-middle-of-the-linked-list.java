a1691261af15a46f54b6cb616c64f4486758dfa2
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-06-14 00:48:11 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (5.58%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -9,16 +9,16 @@
  * }
  */
 class Solution {
+
     public ListNode middleNode(ListNode head) {
-        ListNode slow=head;
-        ListNode fast=head;
-        while(fast.next!=null){
-            if(fast.next.next==null){
-                slow=slow.next;
-                return slow;
-            }
-            fast=fast.next.next;
-            slow=slow.next;
+        ListNode slow = head;
+        ListNode fast = head;
+        //fast!=null (agar boundary cross kargaya) 
+        //fast.next!=null (agar just boundary pe hai)
+        //then just terminate the while loop, slow stores the required node automatically
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
         return slow;
     }
