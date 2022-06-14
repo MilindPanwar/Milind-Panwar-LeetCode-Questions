6b442714413a814967368457445e2d85e4bd2103
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-06-14 11:35:22 +0530

    Time: 1 ms (99.55%), Space: 45.5 MB (84.01%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -12,18 +12,65 @@
 public class Solution {
 
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        
+        ListNode l1 = headA;
         ListNode l2 = headB;
-        while (l2 != null) {
-            ListNode l1 = headA; //l1 at head at every new iteration
-            while (l1 != null) {
-                if (l1 == l2) {
-                    return l2;
-                }
-                l1 = l1.next;
-            }
-            l2 = l2.next;
+        //STEP ONE : calculate the size of both LL
+        int size1 = 0;
+        int size2 = 0;
+        while (l1 != null) {
+            l1 = l1.next;
+            size1++;
         }
+
+        while (l2 != null) {
+            l2 = l2.next;
+            size2++;
+        }
+
+        //STEP TWO : find the difference between them
+        int diff = 0;
+        ListNode temp1;
+        ListNode temp2;
+        if (size1 >= size2) {
+            diff = size1 - size2;
+            temp1 = headB;
+            temp2 = headA;
+
+            //STEP THREE : assign temp at difference
+            for (int i = 0; i < diff; i++) {
+                temp2 = temp2.next;
+            }
+
+            //STEP FOUR : traverse for both
+            //compare both node if found return at that moment
+            while (temp1 != null && temp2 != null) {
+                if (temp1 == temp2) {
+                    return temp2;
+                }
+                temp1 = temp1.next;
+                temp2 = temp2.next;
+            }
+        } else if (size2 > size1) {
+            diff = size2 - size1;
+            temp1 = headA;
+            temp2 = headB;
+            //STEP THREE : assign temp at difference
+
+            for (int i = 0; i < diff; i++) {
+                temp2 = temp2.next;
+            }
+
+            //STEP FOUR : traverse for both
+            //compare both node if found return at that moment
+            while (temp1 != null && temp2 != null) {
+                if (temp1 == temp2) {
+                    return temp2;
+                }
+                temp1 = temp1.next;
+                temp2 = temp2.next;
+            }
+        }
+        //if both reaches null automatically return null
         return null;
     }
 }
