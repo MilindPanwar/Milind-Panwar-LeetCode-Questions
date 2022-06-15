c0aada67f7ddb74276eac2dbcd835ab4aeb32d3d
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-06-15 09:08:35 +0530

    Time: 1 ms (74.09%), Space: 43 MB (47.40%) - LeetHub

--- 61-rotate-list/61-rotate-list.java
+++ 61-rotate-list/61-rotate-list.java
@@ -10,29 +10,27 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head==null || head.next==null){
+        if(head==null || head.next==null || k==0){
             return head;
         }
-        ListNode len=head;
-        int size=0;
         
-        //calculating size to do k modulo size
-        //to avoid multiple iterations which are of no use
-        while(len!=null){
+        //calculating the size of the LL
+        int size=1;
+        ListNode temp=head;
+        while(temp.next!=null){
             size++;
-            len=len.next;
+            temp=temp.next;
         }
-        for(int i=0;i<k%size;i++){
-            ListNode temp=head;
-            //placing temp and 2nd last Node
-            while(temp.next.next!=null) temp=temp.next;
-            
-            //placing end on the Last Node
-            ListNode end=temp.next;
-            temp.next=null; //2nd last node points to null now
-            end.next=head; //the last node point to head now
-            head=end; //head points to new head whose name is end
+        //doing k modulo size to avoid multiple useless iterations
+        k=k%size;
+        int nk=size-k; //value of kth node from the back
+        temp.next=head; //assigning it to head for new iteration
+        while(nk>0){
+            temp=temp.next;
+            nk--;
         }
-        return head;
+        head=temp.next; //moving head to node which is right after kth node
+        temp.next=null; //breaking the node by assigning the kth node null
+        return head; 
     }
 }
