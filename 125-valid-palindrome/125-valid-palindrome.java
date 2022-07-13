921f2d11970f88696af72af390a680dba8e8d7ee
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-07-13 21:47:26 +0530

    Time: 6 ms (66.67%), Space: 44.6 MB (40.02%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,39 +1,24 @@
 class Solution {
-
-    public boolean isPalindrome(String s) {
-        if (s.isEmpty()) {
-            return true;
-        }
-        int head = 0;
-        int tail = s.length() - 1;
-        char chead, ctail;
-        while (head <= tail) {
-             chead = s.charAt(head);
-             ctail = s.charAt(tail);
-            //if head element is space, sign, etc
-            if (!Character.isLetterOrDigit(chead)) {
-                head++;
-                
+    public boolean isPalindrome(String sa) {
+String s=   sa.toLowerCase();
+        int lo=0;
+        int hi=s.length()-1;
+        
+        while(lo<hi){
+            while(lo<hi && !Character.isLetterOrDigit(s.charAt(lo))){
+                lo++;
             }
-            //if tail element is space, sign, etc
-            else if (!Character.isLetterOrDigit(ctail)) {
-                tail--;
-            } 
-            //if element is not space, sign etc
-            else {
-                //checking the start and end
-                //if start element ie head is not equal to end element
-                //returning false
-                if (Character.toLowerCase(chead) != Character.toLowerCase(ctail)) {
-                    return false;
-                }
-                head++;
-                tail--;
+            while(lo<hi && !Character.isLetterOrDigit(s.charAt(hi))){
+                hi--;
             }
+            if(s.charAt(lo)!=s.charAt(hi)){
+                return false;
+            }
+            lo++;
+            hi--;
+            
         }
-        //this means tail became equal to head
-        //traversed through the string
-        //and no mismatch between head char and tail char found
         return true;
+        
     }
 }
