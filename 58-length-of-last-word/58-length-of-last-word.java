ac9d3cc5f725f890ada9cd0a34a6bd9b7d5fd23a
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-07-13 20:55:46 +0530

    Time: 4 ms (9.45%), Space: 42.9 MB (6.74%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -1,26 +1,8 @@
 class Solution {
 
     public int lengthOfLastWord(String s) {
-        int length = 0;
-        //reverse loop
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) != ' ') {
-                //if element at index is not space
-                length++;
-            }
-            //entering here means we found a space
-            //that can be a first space
-            //or may be a space after a whole word
-            else {
-                if (length > 0) {
-                    //if we already incremented length before
-                    //means we already traversed a whole word
-                    //and now we got a space
-                    //so return the length
-                    return length;
-                }
-            }
-        }
-        return length;
+        String[] words = s.split("\\s+");
+        int len = words[words.length - 1].length();
+        return len;
     }
 }
