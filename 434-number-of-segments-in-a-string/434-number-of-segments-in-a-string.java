4b5e0221d592b684ad0b2533fdf9911c7d85890f
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-07-14 10:28:12 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (65.25%) - LeetHub

--- 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.java
+++ 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.java
@@ -1,20 +1,28 @@
 class Solution {
 
     public int countSegments(String s) {
-        int segment = 0;
-        char[] charr = s.toCharArray();
-        for (int i = 0; i < charr.length; i++) {
-            if (charr[i] != ' ') {
-                segment++;
-            }
-            //count one word 
-            while (i < charr.length && charr[i] != ' ') {
-                //if one word is completed
-                //assign value of i to the end of the word
-                //word by word segment is counted
-                i++;
-            }
+        if (s.length() == 0) {
+            return 0;
         }
-        return segment;
+        int count = 0;
+        int seg = 0;
+
+        int i = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) != ' ') {
+                count++;
+            } else {
+                if (count > 0) {
+                    seg++;
+                    count = 0;
+                }
+            }
+            if (i == s.length() - 1 && s.charAt(i) != ' ') {
+                seg++;
+            }
+
+            i++;
+        }
+        return seg;
     }
 }
