514309262f08bc1db495fd61717f107ae39132d3
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-07-15 09:16:28 +0530

    Time: 1 ms (100.00%), Space: 40.6 MB (92.89%) - LeetHub

--- 520-detect-capital/520-detect-capital.java
+++ 520-detect-capital/520-detect-capital.java
@@ -1,35 +1,14 @@
 class Solution {
 
     public boolean detectCapitalUse(String word) {
-        int c = 0;
-        int s = 0;
+        if (word.toUpperCase().equals(word)) return true;
+        if (word.toLowerCase().equals(word)) return true;
 
-        if (word.charAt(0) >= 65 && word.charAt(0) <= 90) {
-            c = 1;
-            s=1;
-        }
-        if (word.charAt(0) >= 97 && word.charAt(0) <= 122) {
-            s = 1;
-        }
-        if (word.length() == 1) {
-            if (c == 1 && word.charAt(0) >= 65 && word.charAt(0) <= 90) {
-                return true;
-            }
-            if (s == 1 && word.charAt(0) >= 97 && word.charAt(0) <= 122) {
+        if (Character.isUpperCase(word.charAt(0))) {
+            if (word.substring(1).toLowerCase().equals(word.substring(1))) {
                 return true;
             }
         }
-
-        for (int i =1 ; i < word.length(); i++) {
-            if (word.charAt(i) >= 65 && word.charAt(i) <= 90) {
-                c++;
-            } else {
-                s++;
-            }
-        }
-       
-        if (c == word.length() || s == word.length()) {
-            return true;
-        } else return false;
+        return false;
     }
 }
