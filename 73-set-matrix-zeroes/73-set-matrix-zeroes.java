c7eb6d52925185f6acf7b510f2ff62be31c3d272
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-08-22 22:52:42 +0530

    Time: 2 ms (47.20%), Space: 54.8 MB (6.13%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,30 +1,28 @@
+
 class Solution {
 
-    public void setZeroes(int[][] matrix) {
-        int row = matrix.length;
-        int col = matrix[0].length;
+    public void setZeroes(int[][] m) {
+        int[][] d1 = new int[1][m[0].length];
+        int[][] d2 = new int[m.length][1];
 
-        int col0 = 1; //for matrix[0][0]
-
-        for (int i = 0; i < row; i++) {
-            if (matrix[i][0] == 0) col0 = 0; //if col=0 in any row, set col0=0;
-            for (int j = 1; j < col; j++) {
-                //starting from the second col, as operatn for first col has been done.
-                if (matrix[i][j] == 0) {
-                    matrix[0][j] = 0; //set topmost 0
-                    matrix[i][0] = 0; //set leftmost 0
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0;j< m[0].length; j++) {
+                if (m[i][j] == 0) {
+                    d1[0][j] = -1;
+                    d2[i][0] = -1;
                 }
             }
         }
-        for (int i = row - 1; i >= 0; i--) {
-            for (int j = col - 1; j >= 1; j--) {
-                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
-                    matrix[i][j] = 0; //set current element 0
+
+        for (int i = 0; i<m.length; i++) {
+            for (int j = 0;j< m[0].length; j++) {
+                if (d1[0][j] == -1 || d2[i][0] == -1) {
+                    m[i][j] = 0;
                 }
-            }
-            if (col0 == 0) { //operation for mat[0][0]
-                matrix[i][0] = 0; //if any first col was 0, then mat00 is set to 0
             }
         }
     }
 }
+
+
+
