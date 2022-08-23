a07e4bb65a229295ebe86a7dcf9dd502e135df0b
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-08-23 12:57:50 +0530

    Time: 1 ms (90.33%), Space: 50 MB (83.13%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.java
+++ 566-reshape-the-matrix/566-reshape-the-matrix.java
@@ -1,23 +1,25 @@
 class Solution {
 
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-        
-//length of row
         int m = mat.length;
-//length of column
         int n = mat[0].length;
-//if dimensions are not equal then
-        if(m*n!=r*c){
+
+        if (m * n != r * c) {
             return mat;
         }
-//new array for reshaped array
-        int[][] arr = new int[r][c];
-//loop till new array dimension which is r into c
-        for (int i = 0; i < r * c; i++) {
-// i/c because it makes 1 row as a result
-// i%c because it makes
-            arr[i / c][i % c] = mat[i / n][i % n];
+        int[][] rc = new int[r][c];
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                rc[x][y] = mat[i][j];
+                y++;
+                if (y == c) {
+                    x++;
+                    y = 0;
+                }
+            }
         }
-        return arr;
+        return rc;
     }
 }
