c38c80755429301b3c807acf10acac168f47cf5e
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-08-23 15:35:56 +0530

    Time: 1 ms (100.00%), Space: 43.3 MB (89.67%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -1,22 +1,30 @@
 class Solution {
-
     public int findUnsortedSubarray(int[] nums) {
-        // finding end point of subarray
-        int end = -1;
-        int max = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (max > nums[i]) {
-                end = i;
-            } else max = nums[i];
+        int low=0;
+        int hi=nums.length-1;
+        
+        while(low+1<nums.length && nums[low]<=nums[low+1]){
+            low++; //finding the breakpoint or the "start" index of subbarray
         }
-        // finding start point of subarray
-        int st = 0;
-        int min = nums[nums.length - 1];
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (min < nums[i]) {
-                st = i;
-            } else min = nums[i];
+        while(hi-1>0 && nums[hi]>=nums[hi-1]){
+            hi--; //finding the breakpoint or the "end" index of subbarray
         }
-        return end - st + 1;
+        if(low==nums.length-1){//iska matlab, Poora array sorted hai.
+            return 0; 
+        }
+        int subMin= Integer.MAX_VALUE;
+        int subMax= Integer.MIN_VALUE;
+        for(int i=low;i<=hi;i++){
+            subMin=Math.min(subMin, nums[i]); //subaray mein min element
+            subMax=Math.max(subMax, nums[i]); //subarray mein max element
+        }
+        while(low-1>=0 && nums[low-1]>subMin){ //low se 0 tak jaega
+            low--; 
+        }
+        while(hi+1<=nums.length-1 && nums[hi+1]<subMax){ //hi se end tak jaega
+            hi++;
+        }
+        return hi-low+1; //indexing 0 se hai islie plus 1
+        
     }
 }
