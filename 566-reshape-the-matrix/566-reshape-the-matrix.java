cd2d386a0afdd8c7d5e02a2233382f9bd81d150e
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-08-23 13:54:43 +0530

    Time: 1 ms (90.33%), Space: 50.6 MB (65.11%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.java
+++ 566-reshape-the-matrix/566-reshape-the-matrix.java
@@ -1,30 +1,23 @@
 class Solution {
 
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-        int m = mat.length;
-        int n = mat[0].length;
+        int m = mat.length; //row ki length
+        int n = mat[0].length; //column ki length
 
         if (m * n != r * c) {
-            return mat; // Question mein ye rule diya hua hai 4th paragraph mein - "If the ...Otherwise, output the original matrix."
+            return mat; // original matrix aur new matrix ke pas same number of elements nahi hain to return karvado original matrix ( as per question, read paragraph 4)
         }
 
-        int[][] rc = new int[r][c];
-        int row = 0; // naya variable row ke liye
-        int column = 0; // naya variable column ke liye
+        int total_Elements = r * c; //total elements in new matrix
 
-        //ek simple nested for loop original matrix mein traverse karne ke liye
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                rc[row][column] = mat[i][j]; // value assign kardo
+        int[][] result = new int[r][c];
+        for (int i = 0; i < total_Elements; i++) { //0 se total_Elements tak loop
+            //MOST IMPORTANT LOGIC HERE : Mod wala logic pen aur paper lekar khudse calculate karo, har iteration ke liye MOD aur DIVIDE kaise horaha hai ye paper pe calculate karke dekho TAB HI SAMAJH AYEGA!
 
-                column++;
-                if (column == c) { //agar column variable row ke last column pe hai to "if" ke andar ghusjaega code
-                    row++; //row ko 1 se bada do taki next row ke traverse karsake hum
-                    column = 0; // column ko 0 set kardo taki nayi row ke 0 index se shuru ho naya traversal
-                }
-            }
+            result[i / c][i % c] = mat[i / n][i % n];
+            // Firbhi Nahi Samajh aaye to DM karo LinkedIn ya Instagram pe.. @MilindPanwar
+
         }
-        return rc; // return kardo apna "rc" naam ka array
+        return result;
     }
 }
-//badhai ho aapne successfully ye code karlia hai, Jai Mata Di...
