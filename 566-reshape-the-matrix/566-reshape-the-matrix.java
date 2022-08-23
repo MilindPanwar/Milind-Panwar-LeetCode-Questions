277d4d6687de427b219c89c593d7134b01e80fbe
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-08-23 13:44:03 +0530

    Time: 1 ms (90.33%), Space: 50.9 MB (31.93%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.java
+++ 566-reshape-the-matrix/566-reshape-the-matrix.java
@@ -5,25 +5,26 @@ class Solution {
         int n = mat[0].length;
 
         if (m * n != r * c) {
-            return mat; //as per a rule given in question
+            return mat; // Question mein ye rule diya hua hai 4th paragraph mein - "If the ...Otherwise, output the original matrix."
         }
 
         int[][] rc = new int[r][c];
-        int row = 0; // new counter for row updation 
-        int column = 0; // new counter for column updation
-        
+        int row = 0; // naya variable row ke liye
+        int column = 0; // naya variable column ke liye
+
+        //ek simple nested for loop original matrix mein traverse karne ke liye
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                
-                rc[row][column] = mat[i][j];
-                
-                column++; 
-                if (column == c) { //if counter column reaches end of row
-                    row++; //update row by 1
-                    column = 0; //set column to 0 
+                rc[row][column] = mat[i][j]; // value assign kardo
+
+                column++;
+                if (column == c) { //agar column variable row ke last column pe hai to "if" ke andar ghusjaega code
+                    row++; //row ko 1 se bada do taki next row ke traverse karsake hum
+                    column = 0; // column ko 0 set kardo taki nayi row ke 0 index se shuru ho naya traversal
                 }
             }
         }
-        return rc;
+        return rc; // return kardo apna "rc" naam ka array
     }
 }
+//badhai ho aapne successfully ye code karlia hai, Jai Mata Di...
