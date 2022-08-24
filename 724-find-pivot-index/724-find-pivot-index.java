5b6b98b9a9ca5477bf1152df8f2ed2e9294d0380
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-08-24 20:09:29 +0530

    Time: 2 ms (78.94%), Space: 52.7 MB (40.30%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -1,20 +1,34 @@
 class Solution {
 
     public int pivotIndex(int[] nums) {
-        if (nums.length == 1) return 1; //length is 1, index is 1, so return 1
-
-        int tsum = 0; // calculating total sum of array
-        for (int i = 0; i < nums.length; i++) {
-            tsum += nums[i];
+        if (nums.length< 1) {
+            return 1;
         }
 
-        int lsum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            tsum = tsum - nums[i]; //right sum
-            if (tsum == lsum) return i; //if lsum=rsum return the index
-            lsum = lsum + nums[i]; //left sum
+        int totalsum = 0; //array ke sare elements ko add karna hai isme
+        for (int i : nums) {
+            totalsum += i; //totalsum mei array sum store kardo
         }
-        //if no index exist return -1
+
+        int leftsum = 0; //left sum ke liye
+        for (int i = 0; i < nums.length; i++) { //loop 0 se end tak
+            //ye right sum ki tarah kaam karega
+            totalsum = totalsum - nums[i];
+
+            //jahan left aur right equal hogaye...
+            if (leftsum==totalsum) {
+                //...wo index return kardo
+                return i;
+            }
+
+            //ye left sum mei values add karne ke liye hai
+            //leftsum ki value badaate chalo
+            leftsum = leftsum + nums[i];
+        }
+
+        //jab left aur right dono equal aaye hi nahi
+        //matlabv pivot index hai hi nahi
+        //to -1 return karvado
         return -1;
     }
 }
