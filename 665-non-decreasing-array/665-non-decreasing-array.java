bfb1090cc78a3e44e7e7b001c11e49ee93f737ed
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-08-24 14:12:20 +0530

    Time: 2 ms (33.97%), Space: 54.6 MB (14.41%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.java
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -1,26 +1,28 @@
 class Solution {
 
     public boolean checkPossibility(int[] nums) {
-        int count = 0;
-        int k = 0;
+        int modi = 0;
+
+        //loop 1 se chalega kyuki n-1 bhi check karenge
         for (int i = 1; i < nums.length; i++) {
             if (nums[i - 1] > nums[i]) {
-                count++;
-                k = i;
+                if (modi == 1) { //mod agar 1 hua to return kardo false
+                    return false;
+                }
+                modi++;
+                //MOST IMPORTANT
+                //i<2 kyuki loop 1 se end tak chalraha hai
+                //aur if statement mei bas nums[i-2]<=nums[i] nahi rakhsakte
+                //kyuki nums[i-2] out of bound hojaega 2 se chote i ke liye (i<2)
+                if (i < 2 || nums[i - 2] <= nums[i]) {
+                    nums[i - 1] = nums[i]; //agar nums[i] se left mei doosra element chota hai
+                } else {
+                    nums[i] = nums[i - 1]; //agar nums[i] se left mei doosra element bada hai
+                }
             }
         }
-        if (count == 0) {
-            return true;
-        }
-        if (count > 1) {
-            return false;
-        }
-        if (k == 1 || k == nums.length - 1) {
-            return true;
-        }
-        if (nums[k + 1] < nums[k - 1] && nums[k] < nums[k - 2]) {
-            return false;
-        }
+
+        //yaha tak agya code matlab array non-decreasing hosakta hai
         return true;
     }
 }
