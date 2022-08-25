bebbf562edc1b702236721d0518925ef7e3c0424
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-08-25 23:12:54 +0530

    Time: 5 ms (74.47%), Space: 46.5 MB (53.26%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,19 +1,26 @@
 class Solution {
 
     public boolean canConstruct(String ransomNote, String magazine) {
-        StringBuilder ran = new StringBuilder(ransomNote);
-        StringBuilder mag = new StringBuilder(magazine);
-        int i = 0;
-        while (i < ran.length()) {
-            char ch = ran.charAt(i);
-            String ctos= String.valueOf(ch);
-            int ind = mag.indexOf(ctos);
-            if (ind != -1) {
-                mag.deleteCharAt(ind);
-            } else {
+        //char array for every alphabet
+        //separate space for every alphabet
+        int[] charr = new int[26];
+
+        for (int c : magazine.toCharArray()) {
+            //alphabet from magazine stored in c
+            //increment the value of that particular element in char array
+            //index of that element is searched and incremented in char array
+            charr[c - 'a'] += 1;
+        }
+        for (int c : ransomNote.toCharArray()) {
+            //if char array have zero value for a particular alphabet
+            //at its destined index
+            //then return false
+            if (charr[c - 'a'] == 0) {
                 return false;
             }
-            i++;
+            //if char array have some value at index of given character
+            //subtract that value
+            charr[c - 'a'] -= 1;
         }
         return true;
     }
