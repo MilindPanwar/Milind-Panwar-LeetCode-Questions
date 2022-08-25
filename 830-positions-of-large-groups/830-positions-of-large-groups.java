89a4983971df1c62e9141dfc0abd538d21720230
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-08-26 01:13:53 +0530

    Time: 3 ms (46.85%), Space: 44.5 MB (80.88%) - LeetHub

--- 830-positions-of-large-groups/830-positions-of-large-groups.java
+++ 830-positions-of-large-groups/830-positions-of-large-groups.java
@@ -1,25 +1,28 @@
 class Solution {
+
     public List<List<Integer>> largeGroupPositions(String s) {
-        List<List<Integer>> result = new ArrayList<>();
-        
-        int i=0;
-        int j=0;
-        int len= s.length();
-        while(i<len){
-            char ch = s.charAt(i);
-            j=i;
-            while(j<len && ch==s.charAt(j)){
-                j++;
+        //list of list banao jo return karni hogi
+        List<List<Integer>> list = new ArrayList<>();
+
+        //ye starting index hai
+        int i = 0;
+        int len = s.length();
+
+        while (i < len) { //jab "i" , "len" ke equal ya bada hojae to break
+            char c = s.charAt(i); // "i"th character "c" mei daldo
+            int j = i; //ye "j" char ko count karega
+            while (j < len && c == s.charAt(j)) { //jabtak j milraha hai...
+                j++; //...tabtak j mei plus 1 karte jao
             }
-            if(j-i>=3){
-                List<Integer> list= new ArrayList<>();
-                list.add(i);
-                list.add(j-1);
-                result.add(list);
+            if (j - i >= 3) { //uss char ki length
+                // j-i karna pada taaki length nikal paaye
+                List<Integer> l1 = new ArrayList<>(); //nayi list banap
+                l1.add(i); // i add kardo (start index)
+                l1.add(j - 1); // (j-i) add kardo( end index)
+                list.add(l1); //dynamic arraylist mein daldo
             }
-            i=j;
+            i = j; //i ab waha se chalega jaha naya char hai
         }
-        return result;
-        
+        return list;
     }
 }
