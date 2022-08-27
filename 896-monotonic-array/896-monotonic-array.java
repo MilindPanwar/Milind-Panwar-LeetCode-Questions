dc4d52c21276c9dffb25eec39ce524b5e9be0f3a
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-08-27 15:58:12 +0530

    Time: 3 ms (67.69%), Space: 93.3 MB (29.20%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.java
+++ 896-monotonic-array/896-monotonic-array.java
@@ -1,31 +1,39 @@
 class Solution {
 
     public boolean isMonotonic(int[] nums) {
-        int a = nums.length;
-        int b = nums.length;
-        if (nums.length == 1) {
+        if (nums.length == 1 || nums.length == 2) {
             return true;
         }
-        if (nums[0] <= nums[1]) {
+        
+        int j = 0;
+        if (nums[0] == nums[1]) {
+            while (j < nums.length - 1 && nums[j] == nums[j + 1]) {
+                j++;
+            }
+        }
+        int c1 = 0;
+        if (j < nums.length - 1 && nums[j] < nums[j + 1]) {
+            c1++;
+        }
+        if (c1 > 0) {
             for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] <= nums[i + 1]) {
-                    a--;
-                    if (a == 1) {
-                        return true;
-                    }
+                if (nums[i] == nums[i + 1]) {
+                    continue;
+                }
+                if (nums[i] > nums[i + 1]) {
+                    return false;
+                }
+            }
+        } else {
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] == nums[i + 1]) {
+                    continue;
+                }
+                if (nums[i] < nums[i + 1]) {
+                    return false;
                 }
             }
         }
-        if (nums[0] >= nums[1]) {
-            for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] >= nums[i + 1]) {
-                    b--;
-                    if (b == 1) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
+        return true;
     }
 }
