d6d2eef0bc148ac63fee0afa539ab41cb0e33115
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-08-27 15:39:33 +0530

    Time: 23 ms (55.83%), Space: 67.9 MB (42.31%) - LeetHub

--- 888-fair-candy-swap/888-fair-candy-swap.java
+++ 888-fair-candy-swap/888-fair-candy-swap.java
@@ -1,30 +1,26 @@
 class Solution {
 
     public int[] fairCandySwap(int[] a, int[] b) {
-        int tsum1 = 0;
-        int tsum2 = 0;
+        HashSet<Integer> hs = new HashSet();
+        int sum_a = 0;
         for (int i : a) {
-            tsum1 += i;
+            sum_a += i;
+            hs.add(i);
         }
-        for (int i : b) {
-            tsum2 += i;
-        }
-        
-        int s1 = 0;
-        int s2 = 0;
-        int[] arr = new int[2];
 
-        for (int i=0; i < a.length; i++) {
-            for (int j=0; j < b.length; j++) {
-                s1 = b[j] + tsum1 - a[i];
-                s2 = a[i] + tsum2 - b[j];
-                if (s1 == s2) {
-                    arr[0] = a[i];
-                    arr[1] = b[j];
-                    break;
-                }
+        int sum_b = 0;
+        for (int i : b) {
+            sum_b += i;
+        }
+
+        int diff = (sum_a - sum_b) / 2;
+
+        for (int i : b) {
+            int target = i + diff;
+            if (hs.contains(target)) {
+                return new int[] { target, i };
             }
         }
-        return arr;
+        return null;
     }
 }
