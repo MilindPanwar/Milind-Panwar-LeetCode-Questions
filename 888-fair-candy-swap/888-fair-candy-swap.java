28da366348578cd4e45486ef03ee9aadfedd56a2
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-08-27 14:58:20 +0530

    Time: 1207 ms (6.24%), Space: 44.3 MB (89.95%) - LeetHub

--- 888-fair-candy-swap/888-fair-candy-swap.java
+++ 888-fair-candy-swap/888-fair-candy-swap.java
@@ -1,28 +1,30 @@
 class Solution {
 
-    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
-        Set<Integer> set = new HashSet();
-        int[] result= new int[2];
-        int ta = 0;
-        int tb = 0;
-        
-        for (int a : aliceSizes) {
-            ta += a;
+    public int[] fairCandySwap(int[] a, int[] b) {
+        int tsum1 = 0;
+        int tsum2 = 0;
+        for (int i : a) {
+            tsum1 += i;
         }
-        for (int b : bobSizes) {
-            tb += b;
-            set.add(b);
+        for (int i : b) {
+            tsum2 += i;
         }
-        int delta = (tb - ta) / 2;
         
-        for (int a : aliceSizes) {
-            if(set.contains(a+delta)){
-                result[0]=a;
-                result[1]=a+delta;
-                return result;
+        int s1 = 0;
+        int s2 = 0;
+        int[] arr = new int[2];
+
+        for (int i=0; i < a.length; i++) {
+            for (int j=0; j < b.length; j++) {
+                s1 = b[j] + tsum1 - a[i];
+                s2 = a[i] + tsum2 - b[j];
+                if (s1 == s2) {
+                    arr[0] = a[i];
+                    arr[1] = b[j];
+                    break;
+                }
             }
-            
         }
-        return null;
+        return arr;
     }
 }
