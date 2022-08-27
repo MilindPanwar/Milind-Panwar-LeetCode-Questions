8ebf37fea8f68a1f47d84fb17984e9f83d210cc0
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-08-27 16:09:20 +0530

    Time: 4 ms (50.15%), Space: 93.3 MB (35.75%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.java
+++ 896-monotonic-array/896-monotonic-array.java
@@ -1,39 +1,20 @@
 class Solution {
 
     public boolean isMonotonic(int[] nums) {
-        if (nums.length == 1 || nums.length == 2) {
+        boolean inc = false;
+        boolean dec = false;
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i]<nums[i-1]){
+                dec=true;
+            }
+            if(nums[i]>nums[i-1]){
+                inc=true;
+            }
+        }
+        if(inc && dec){
+            return false;
+        }else{
             return true;
         }
-        
-        int j = 0;
-        if (nums[0] == nums[1]) {
-            while (j < nums.length - 1 && nums[j] == nums[j + 1]) {
-                j++;
-            }
-        }
-        int c1 = 0;
-        if (j < nums.length - 1 && nums[j] < nums[j + 1]) {
-            c1++;
-        }
-        if (c1 > 0) {
-            for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] == nums[i + 1]) {
-                    continue;
-                }
-                if (nums[i] > nums[i + 1]) {
-                    return false;
-                }
-            }
-        } else {
-            for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] == nums[i + 1]) {
-                    continue;
-                }
-                if (nums[i] < nums[i + 1]) {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 }
