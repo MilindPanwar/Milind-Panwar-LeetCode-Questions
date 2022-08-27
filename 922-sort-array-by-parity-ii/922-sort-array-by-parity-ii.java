3a994e1b90162716e31816b8f04cd1f291489cbb
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-08-27 16:50:27 +0530

    Time: 4 ms (62.03%), Space: 55.7 MB (7.59%) - LeetHub

--- 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.java
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.java
@@ -1,24 +1,31 @@
 class Solution {
 
     public int[] sortArrayByParityII(int[] nums) {
-        int i = 0, j = 1, n = nums.length;
-        while (i < n && j < n) {
-            if (i < n && nums[i] % 2 == 0) {
-                i += 2;
-            }
-            if (j < n && nums[j] % 2 == 1) {
-                j += 2;
-            }
-            if (i < n && j < n) {
-                swap(nums, i, j);
+        int p = 1;//1 se shuru matlab odd ke liye
+        for (int i = 0; i < nums.length; i += 2) { //0 se shuru matlab even ke liye
+            //increment bhi har baar +2 karke hoga kyuki even ke liye h
+            
+            //ye odd index pe hi chalega humesha
+            //odd index pe even value aye to wahan break hojaega
+            while (p < nums.length && nums[p] % 2 == 1) {
+                p += 2;//+2 kar rahe hai kyuki 1 se shuru hua hai
+                // "p" odd tha aur odd hi rahega
+            }//odd index pe even value mile to ye rukjaega aur
+            // j mei odd index store kardega jisme even value hai
+            
+            if (nums[i] % 2 == 1) {//even index pe odd value mile to 
+                
+                //nums[i] even index & odd value
+                //nums[p] odd index & even value
+                
+                //in dono ko swap kardo, bas yahi to karna hai
+                int temp = nums[i];
+                nums[i] = nums[p];
+                nums[p] = temp;
+                p = p + 2;
             }
         }
+        //return karvado aur mauj karo
         return nums;
-    }
-
-    public void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 }
