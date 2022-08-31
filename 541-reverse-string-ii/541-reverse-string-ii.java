f52e09905b86fcff6634ebe7dbf2525de9b48f16
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-08-31 19:36:34 +0530

    Time: 2 ms (70.65%), Space: 44 MB (51.07%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.java
+++ 541-reverse-string-ii/541-reverse-string-ii.java
@@ -1,32 +1,22 @@
-                /*ALGORITHM
-    1. make copy of string in stringbuilder
-    2. while loop for incrementing k
-    3. inner while loop for swapping the elements
-    
-*/
-
 class Solution {
 
     public String reverseStr(String s, int k) {
-        StringBuilder sb = new StringBuilder(s);
-
-        int l = 0;
-        while (l < sb.length()) {
-            int i = l;
-            
-            //the most important step is to initialize "j" everytime
-            int j = Math.min(l+k, sb.length())-1;
-            //l+k works if elements equal to k or greater than k is left
-            //sb.length works if like elements left is 2 and k is 3
-            while (i < j) {
-                StringBuilder t = new StringBuilder(sb);
-                sb.setCharAt(i, t.charAt(j));
-                sb.setCharAt(j, t.charAt(i));
-                i++;
-                j--;
-            }
-            l = l + (2 * k);
+        char[] arr = s.toCharArray();
+        int i = 0, n = s.length();
+        while (i < n) {
+            reverse(arr, i, Math.min(i + k - 1, n - 1));
+            i += k * 2;
         }
-        return sb.toString();
+        return new String(arr);
+    }
+
+    public void reverse(char[] arr, int i, int j) {
+        while (i < j) {
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
     }
 }
