c00ac7a077ede0f8e248550e2402a8804a825034
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-08-31 13:30:46 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (47.09%) - LeetHub

--- 521-longest-uncommon-subsequence-i/521-longest-uncommon-subsequence-i.java
+++ 521-longest-uncommon-subsequence-i/521-longest-uncommon-subsequence-i.java
@@ -1,3 +1,10 @@
+/* 
+                ALGORITHM
+1.  longest uncommon subsequence means if the whole string is different
+    then it is called longest uncommon subsequnce
+2.  just compare both string and if equal return -1 else return Math.max height of a or b
+*/
+
 class Solution {
 
     public int findLUSlength(String a, String b) {
