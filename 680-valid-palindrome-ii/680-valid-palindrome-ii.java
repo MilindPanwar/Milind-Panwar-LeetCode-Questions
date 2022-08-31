96187f9aab67a23934383735bce3174f44a6edbb
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-08-31 18:54:15 +0530

    Time: 9 ms (86.19%), Space: 55.3 MB (17.69%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -1,36 +1,25 @@
-/*              ALGORITHM
-    1. compare each element from i=0 & j=s.length()-1
-    2. separate function to check if there are any extra element rather than 1
-    3. funtion runs from i+1 or j-1
-    4. function returns false if there are more than 1 element 
-*/
-
-
 class Solution {
 
     public boolean validPalindrome(String s) {
-        int i = 0;
-        int j = s.length() - 1;
+        int l = 0;
+        int r = s.length() - 1;
 
-        while (i <= j) {
-            if (s.charAt(i) == s.charAt(j)) {
-                i++;
-                j--;
+        while (l < r) {
+            if (s.charAt(l)==s.charAt(r)) {
+                l++;
+                r--;
             } else {
-                return isPal(s, i + 1, j) || isPal(s, i, j - 1);
+                return isPal(s, l + 1, r) || isPal(s, l, r - 1);
             }
         }
         return true;
     }
 
-    public boolean isPal(String s, int i, int j) {
-        //funtion returns true only if there is only one extra element which is stopping palindrome
-        //if it returns false that means there are more than one elements 
-        //if more than 1 element is present that means it cannot become a palindrome
-        while (i <= j) {
-            if (s.charAt(i) == s.charAt(j)) {
-                i++;
-                j--;
+    static boolean isPal(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l)==s.charAt(r)) {
+                l++;
+                r--;
             } else {
                 return false;
             }
