8124e85030258f9f8e0bd5ca5dc37c12f0112c73
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-09-02 13:15:45 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (60.20%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -1,15 +1,14 @@
 class Solution {
 
     public int lengthOfLastWord(String s) {
-        int c = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) != ' ') {
-                c++;
-            } else {
-                if (c > 0) {
-                    return c;
-                }
-            }
+        int p = s.length() - 1;
+        int c=0;
+        while (p>=0 && s.charAt(p) == ' ') {
+            p--;
+        }
+        while(p>=0 && s.charAt(p)!=' '){
+            c++;
+            p--;
         }
         return c;
     }
