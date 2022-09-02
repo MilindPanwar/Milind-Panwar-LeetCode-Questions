b9abe220f5702a3db85d272a699b830d0ab46f4d
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-09-02 13:09:12 +0530

    Time: 4 ms (81.70%), Space: 46.4 MB (50.87%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -5,15 +5,14 @@ class Solution {
         StringBuilder m = new StringBuilder(magazine);
         int i = 0;
         while (i < r.length()) {
-            if (i < r.length()) {
-                char c = r.charAt(i);
-                String ss = String.valueOf(c);
-                if (m.indexOf(ss) != -1) {
-                    m.deleteCharAt(m.indexOf(ss));
-                } else {
-                    return false;
-                }
+            char c = r.charAt(i);
+            String ss = String.valueOf(c);
+            if (m.indexOf(ss) != -1) {
+                m.deleteCharAt(m.indexOf(ss));
+            } else {
+                return false;
             }
+
             i++;
         }
 
