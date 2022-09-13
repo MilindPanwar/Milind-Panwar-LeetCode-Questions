d5de51c1090011986505c9951702fbc4904b210a
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-09-13 09:23:25 +0530

    Time: 2 ms (71.68%), Space: 42.6 MB (41.53%) - LeetHub

--- 520-detect-capital/520-detect-capital.java
+++ 520-detect-capital/520-detect-capital.java
@@ -4,10 +4,8 @@ class Solution {
         if (word.toUpperCase().equals(word)) return true;
         if (word.toLowerCase().equals(word)) return true;
 
-        if (Character.isUpperCase(word.charAt(0))) {
-            if (word.substring(1).toLowerCase().equals(word.substring(1))) {
-                return true;
-            }
+        if (Character.isUpperCase(word.charAt(0)) && word.substring(1).toLowerCase().equals(word.substring(1))) {
+            return true;
         }
         return false;
     }
