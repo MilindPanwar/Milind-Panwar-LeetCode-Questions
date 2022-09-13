fdab12c430d792e93926dd1869274fb8e6f0f6b0
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-09-13 16:13:47 +0530

    Time: 0 ms (100.00%), Space: 42 MB (35.71%) - LeetHub

--- 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.java
+++ 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.java
@@ -1,28 +1,19 @@
 class Solution {
 
     public int countSegments(String s) {
-        if (s.length() == 0) {
+        if(s.length()==0){
             return 0;
         }
-        int count = 0;
-        int seg = 0;
-
-        int i = 0;
-        while (i < s.length()) {
-            if (s.charAt(i) != ' ') {
-                count++;
-            } else {
-                if (count > 0) {
-                    seg++;
-                    count = 0;
-                }
+        int c = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) != ' ' && s.charAt(i + 1) == ' ') {
+                c++;
             }
-            if (i == s.length() - 1 && s.charAt(i) != ' ') {
-                seg++;
-            }
-
-            i++;
         }
-        return seg;
+        if (s.charAt(s.length()-1)!=' ') {
+            c++;
+            
+        }
+        return c;
     }
 }
