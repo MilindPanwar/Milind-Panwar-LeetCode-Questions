688ea39a156583f8dca8e67997e2efad148d0d36
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-10-05 00:26:16 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (82.21%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -9,15 +9,15 @@
  * }
  */
 class Solution {
+
     public ListNode reverseList(ListNode head) {
-        ListNode newHead=null;
-        while(head!=null){
-            //new ListNode "Next" points at what is storedat head.next of previous node
-            ListNode next= head.next; //pointed next to immediate next of head
-            head.next=newHead; //broke the next pointer of head & pnted it to dummy node  
-            newHead=head; //moving dummy node to head
-            head=next; //moving head to next
+        ListNode newNode = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newNode;
+            newNode = head;
+            head = next;
         }
-        return newHead;
+        return newNode;
     }
 }
