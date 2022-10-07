d66bf9f77c76a8bcbfd6469a073c39b7efe2bd13
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-10-08 05:05:21 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (90.86%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -10,17 +10,19 @@
  * }
  */
 public class Solution {
-
     public boolean hasCycle(ListNode head) {
-        ListNode h = head;
-        HashSet<ListNode> hs = new HashSet<ListNode>();
-        while (h != null) {
-            if (hs.contains(h)) {
+        if(head==null){
+            return false;
+        }
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast.next!=null && fast.next.next!=null){
+            
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
                 return true;
-            } else {
-                hs.add(h);
             }
-            h = h.next;
         }
         return false;
     }
