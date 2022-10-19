ed952ee29e761fc788242adfbdfb12fabc9bde1e
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-10-19 12:32:46 +0530

    Time: 2 ms (91.10%), Space: 40.3 MB (89.96%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,48 +2,28 @@ class Solution {
 
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<Character>();
-
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (c == '(' || c == '[' || c == '{') {
                 st.push(c);
             } else {
-                if (st.isEmpty()) {
-                    return false;
-                }
                 switch (c) {
-                    case ')':
-                        if (st.peek() == '(') {
-                            st.pop();
-                            break;
-                        } else {
-                            return false;
-                            
-                        }
-                    case ']':
-                        if (st.peek() == '[') {
-                            st.pop();
-                            break;
-                        } else {
-                            return false;
-                          
-                        }
                     case '}':
-                        if (st.peek() == '{') {
-                            st.pop();
-                            break;
-                        } else {
-                            return false;
-                          
-                        }
+                        if (!st.isEmpty() && st.peek() == '{') st.pop(); else return false;
+
+                        break;
+                    case ')':
+                        if (!st.isEmpty() && st.peek() == '(') st.pop(); else return false;
+                        break;
+                    case ']':
+                        if (!st.isEmpty() && st.peek() == '[') st.pop(); else return false;
+                        break;
                 }
             }
         }
-        if(st.isEmpty()){
-            return true;
-        }else{
-            return false; 
+        if(!st.isEmpty()){
+            return false;
         }
-        
+        return true;
     }
 }
