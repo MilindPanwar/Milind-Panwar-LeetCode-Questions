0c835c824581e751d542182c840dad971045a80a
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-11-02 10:25:49 +0530

    Time: 50 ms (39.46%), Space: 63.3 MB (28.67%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,9 +2,14 @@ class Solution {
 
     public List<List<Integer>> threeSum(int[] nums) {
         Set<List<Integer>> res = new HashSet<>();
+        if (nums.length == 0) return new ArrayList<>(res);
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                // skip same result
+                continue;
+            }
             int left = i + 1;
             int right = nums.length - 1;
 
@@ -22,6 +27,6 @@ class Solution {
                 }
             }
         }
-        return new ArrayList<List<Integer>>(res);
+        return new ArrayList<>(res);
     }
 }
