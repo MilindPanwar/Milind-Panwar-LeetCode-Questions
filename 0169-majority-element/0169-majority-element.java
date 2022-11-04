ee7e0e3abe410f7793a23529e429abb2e53a431c
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-11-04 13:33:01 +0530

    Time: 18 ms (34.95%), Space: 55.9 MB (53.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,7 +1,25 @@
 class Solution {
+
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        //hashmap banao
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         
+        //array ki sari value hashmap mei daldo
+        for (int i = 0; i < nums.length; i++) {
+            //getordefault ka matlab hai, 
+            //element nahi hai to 1 rakho
+            //element pehlai se present hai to uski value get karo aur +1 karke add kardo
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        int max = 0, res = 0;
+        //Map.entry wagehra foreach loop lagane ke liye use kia hai
+        for(Map.Entry<Integer, Integer> val : map.entrySet()) {
+            
+            if (max < val.getValue()) { //max se badi hai frequency
+                max = val.getValue(); //max mei frequency daldo
+                res = val.getKey(); //result mei wo element daldo
+            }
+        }
+        return res;
     }
 }
