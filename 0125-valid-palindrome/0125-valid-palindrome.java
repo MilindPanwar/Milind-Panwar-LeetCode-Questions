11a57492691060d14b13dfd92a41495498c29c92
Milind Panwar <99394240+MilindPanwar@users.noreply.github.com>
2022-11-11 10:10:59 +0530

    Time: 7 ms (66.35%), Space: 43.7 MB (62.26%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,27 +1,23 @@
 class Solution {
-    public boolean isPalindrome(String s) {
-        if(s.isEmpty()){
-            return true;
-        }
-        String lc=s.toLowerCase();
-        StringBuilder sb= new StringBuilder();
-        int sl=s.length();
-        for(int i=0;i<lc.length();i++){
-            char c=lc.charAt(i);
-            if(c>='a' && c<='z' || c>=48 && c<=57){
-                sb.append(c);
+
+    public boolean isPalindrome(String S) {
+        String s = S.toLowerCase();
+        int i = 0, j = s.length() - 1;
+
+        while (i <= j) {
+            char c1 = s.charAt(i);
+            char c2 = s.charAt(j);
+            while (i <= j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
             }
-        }   
-      
-        int st=0;
-        int e=sb.length()-1;
-        while(st<=e){
-            if(sb.charAt(st)==sb.charAt(e)){
-                st++;
-                e--;
-            }else{
+            while (i <= j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+            if (i <= j && s.charAt(i) != s.charAt(j)) {
                 return false;
             }
+            i++;
+            j--;
         }
         return true;
     }
